8d95ac8309280dd0aa9ed3ea5c137a74382791b1
Nikita Volkov <2545442@gmail.com>
2018-10-24 01:01:55 +0300

    commit #01

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,8 @@
 function getImage() {
-	/*var data = "{}";
-	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = true;
-	xhr.addEventListener("readystatechange", function () {
-	  if (this.readyState === this.DONE) {
-	    console.log(this.responseText);
-	    return `
-	    	<img src="http://image.tmdb.org/t/p/w500/hEpWvX6Bp79eLxY1kX5ZZJcme5U.jpg" alt="image here">
-	    `
-	  }
-	});
-	xhr.open("GET", "http://image.tmdb.org/t/p/w500/hEpWvX6Bp79eLxY1kX5ZZJcme5U.jpg");
-	xhr.send(data);*/
-	document.getElementById('image').innerHTML = `
-  	<img src="http://image.tmdb.org/t/p/w500/hEpWvX6Bp79eLxY1kX5ZZJcme5U.jpg" alt="image here">
-  `;
+	if ( event.target.value.length > 30 ) {
+		let imageUrl = event.target.value;
+		document.getElementById('image').innerHTML = `
+	  	<img src="http://image.tmdb.org/t/p/w500${imageUrl}" alt="image here">
+	  `;
+	}
 }
